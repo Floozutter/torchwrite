2490d64d959e8d4f7558fc1194d80368f8b0b451
Floozutter <me@floozutter.site>
2022-01-05 01:56:07 -0800

    get references to visualizer and editor elements

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import "index.css";
 
-console.log("hello, world!");
+const visualizer = document.getElementById("visualizer") as HTMLCanvasElement;
+const editor = document.getElementById("editor") as HTMLTextAreaElement;
