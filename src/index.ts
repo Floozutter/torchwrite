234c5d003322b677fda710a67c69169b2d21eb16
Floozutter <me@floozutter.site>
2022-01-05 18:41:43 -0800

    listen to input events on editor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import "index.css";
 
 const visualizer = document.getElementById("visualizer") as HTMLCanvasElement;
+
 const editor = document.getElementById("editor") as HTMLTextAreaElement;
+editor.addEventListener("input", (e: Event) => {
+    if (!(e instanceof InputEvent && e.target instanceof HTMLTextAreaElement)) { return; }
+    console.log(e.target.value);
+});
