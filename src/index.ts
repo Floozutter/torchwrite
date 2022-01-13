a97d2eb404c211090a0462275c47d8f2d4ac7216
Floozutter <me@floozutter.site>
2022-01-13 13:09:47 -0800

    call visualizer.setSources from the editor listener

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import "index.css";
 import { Visualizer } from "Visualizer";
 
-const _visualizer = new Visualizer(document.getElementById("visualizer") as HTMLCanvasElement);
+const visualizer = new Visualizer(document.getElementById("visualizer") as HTMLCanvasElement);
 
 const editor = document.getElementById("editor") as HTMLTextAreaElement;
 editor.addEventListener("input", (e: Event) => {
     if (!(e instanceof InputEvent && e.target instanceof HTMLTextAreaElement)) { return; }
+    visualizer.setSources((x, z) => x % 16 == 0 && z % 16 == 0 ? 15 : 0);
     console.log(e.target.value);
 });
