b680dcf894e690cb5a6c23c90329cf6e0a5af5fa
Floozutter <me@floozutter.site>
2022-01-16 17:52:21 -0800

    add lightClamp

--- src/light.ts
+++ src/light.ts
@@ -3,3 +3,7 @@ export type Light = 0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15;
 export interface LightPattern {
     (x: number, z: number): Light;
 }
+
+export function lightClamp(n: number): Light {
+    return Math.round(Math.min(Math.max(n, 0), 15)) as Light;
+}
