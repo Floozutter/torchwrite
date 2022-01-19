a300fdce93b54509ba9e2755a91c795208a7361c
Floozutter <me@floozutter.site>
2022-01-18 17:23:58 -0800

    set visualizer sources from editor text

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import "index.css";
 import { Visualizer } from "Visualizer";
+import { lightWrap } from "light";
 
 const visualizer = new Visualizer(document.getElementById("visualizer") as HTMLCanvasElement);
 
 const editor = document.getElementById("editor") as HTMLTextAreaElement;
 editor.addEventListener("input", (e: Event) => {
     if (!(e instanceof InputEvent && e.target instanceof HTMLTextAreaElement)) { return; }
-    visualizer.setSources((x, z) => x % 16 == 0 && z % 16 == 0 ? 15 : 0);
     console.log(e.target.value);
+    // eslint-disable-next-line @typescript-eslint/no-implied-eval
+    const f = new Function("x", "z", e.target.value);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    visualizer.setSources(lightWrap(f as (x: number, z: number) => any));
 });
 
 editor.value = "return x % 16 == 0 && z % 16 == 0 ? 15 : 0;";
