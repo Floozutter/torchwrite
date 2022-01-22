55cf275af1db8014307caf694e1316ac3c23c017
Floozutter <me@floozutter.site>
2022-01-21 22:44:54 -0800

    add light.test.ts

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import("ts-jest/dist/types").InitialOptionsTsJest} */
 module.exports = {
+    injectGlobals: false,
+    moduleDirectories: ["src/", "node_modules/"],
     preset: "ts-jest",
     rootDir: "tests/",
     testEnvironment: "node",
---
+++ tests/light.test.ts
@@ -0,0 +1,10 @@
+import { lightClamp, lightWrap } from "light";
+import { describe, it } from "@jest/globals";
+
+describe(lightClamp.name, () => {
+    it.todo("prevents NaN propagation");
+});
+
+describe(lightWrap.name, () => {
+    it.todo("returns 0 instead of throwing");
+});
