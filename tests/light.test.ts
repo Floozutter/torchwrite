8aa23345f9b286ebaa591e96c94b2070791dc235
Floozutter <me@floozutter.site>
2022-01-24 16:12:02 -0800

    lightClamp should clamp to minimum

--- tests/light.test.ts
+++ tests/light.test.ts
@@ -2,6 +2,12 @@ import { lightClamp, lightWrap } from "light";
 import { describe, it, expect } from "@jest/globals";
 
 describe(lightClamp.name, () => {
+    it("clamps values less than 0 to 0", () => {
+        expect(lightClamp(-Number.EPSILON)).toBe(0);
+        expect(lightClamp(-1)).toBe(0);
+        expect(lightClamp(Number.MIN_SAFE_INTEGER)).toBe(0);
+        expect(lightClamp(-Infinity)).toBe(0);
+    });
     it("prevents NaN propagation by returning 0", () => {
         expect(lightClamp(NaN)).toBe(0);
     });
