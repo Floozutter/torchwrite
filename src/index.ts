11097ee8af3d54a6cf187612aad23de38af4c233
Floozutter <me@floozutter.site>
2022-01-29 06:00:16 -0800

    use evalLightPattern in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "index.css";
 import { Visualizer } from "Visualizer";
-import { lightWrap } from "light";
+import { evalLightPattern } from "light";
 
 const visualizer = new Visualizer(document.getElementById("visualizer") as HTMLCanvasElement);
 
@@ -8,10 +8,11 @@ const editor = document.getElementById("editor") as HTMLTextAreaElement;
 editor.addEventListener("input", (e: Event) => {
     if (!(e instanceof InputEvent && e.target instanceof HTMLTextAreaElement)) { return; }
     console.log(e.target.value);
-    // eslint-disable-next-line @typescript-eslint/no-implied-eval
-    const f = new Function("x", "z", e.target.value);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    visualizer.setSources(lightWrap(f as (x: number, z: number) => any));
+    try {
+        visualizer.setSources(evalLightPattern(e.target.value));
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 editor.value = "return x % 16 == 0 && z % 16 == 0 ? 15 : 0;";
